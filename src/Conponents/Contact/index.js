c79db39ff340ec4fba32bf111b948bc94a443702
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2022-01-08 01:12:24 +0200

    contact section

--- src/Conponents/Contact/index.js
+++ src/Conponents/Contact/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Contact = ()=> {
     return(
-      <div>
-        Contact
+      <div className='container'>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Massage</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+        </div>
       </div>
+
+      
     )
 }
 
